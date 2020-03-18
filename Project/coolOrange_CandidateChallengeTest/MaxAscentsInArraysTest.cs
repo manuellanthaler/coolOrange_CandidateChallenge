b169c0b42f2d2db99c937a89ad398c53131ba79f
coolOrange-Public <git@coolorange.com>
2020-03-18 09:26:43 +0100

    updated challenge-description template
    - Applied coolOrange template to challenge-description
    - Changed UnitTest Assert to Assert.AreEqual

--- Project/coolOrange_CandidateChallengeTest/MaxAscentsInArraysTest.cs
+++ Project/coolOrange_CandidateChallengeTest/MaxAscentsInArraysTest.cs
@@ -1,64 +1,63 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using coolOrange_CandidateChallenge;
 
 namespace coolOrange_CandidateChallengeTest
 {
-    [TestClass]
-    public class MaxAscentsInArraysTest
-    {
-        [TestMethod]
-        public void testMaxAscentsLength_00()
-        {
-            Assert.Equals(0, MaxAscentsInArrays.maxAscent(new int[] { }));
-        }
+	[TestClass]
+	public class MaxAscentsInArraysTest
+	{
+		[TestMethod]
+		public void testMaxAscentsLength_00()
+		{
+			Assert.AreEqual(0, MaxAscentsInArrays.maxAscent(new int[] { }));
+		}
 
-        [TestMethod]
-        public void testMaxAscentsLength_01()
-        {
-            Assert.Equals(1, MaxAscentsInArrays.maxAscent(new int[] { 1 }));
-        }
+		[TestMethod]
+		public void testMaxAscentsLength_01()
+		{
+			Assert.AreEqual(1, MaxAscentsInArrays.maxAscent(new int[] { 1 }));
+		}
 
-        [TestMethod]
-        public void testMaxAscentsLength_02()
-        {
-            Assert.Equals(2, MaxAscentsInArrays.maxAscent(new int[] { 1, 2 }));
-        }
+		[TestMethod]
+		public void testMaxAscentsLength_02()
+		{
+			Assert.AreEqual(2, MaxAscentsInArrays.maxAscent(new int[] { 1, 2 }));
+		}
 
-        [TestMethod]
-        public void testMaxAscentsLength_03()
-        {
-            Assert.Equals(2, MaxAscentsInArrays.maxAscent(new int[] { 1, 2, 1 }));
-        }
+		[TestMethod]
+		public void testMaxAscentsLength_03()
+		{
+			Assert.AreEqual(2, MaxAscentsInArrays.maxAscent(new int[] { 1, 2, 1 }));
+		}
 
-        [TestMethod]
-        public void testMaxAscentsLength_04()
-        {
-            Assert.Equals(2, MaxAscentsInArrays.maxAscent(new int[] { 1, 2, 1, 2 }));
-        }
+		[TestMethod]
+		public void testMaxAscentsLength_04()
+		{
+			Assert.AreEqual(2, MaxAscentsInArrays.maxAscent(new int[] { 1, 2, 1, 2 }));
+		}
 
-        [TestMethod]
-        public void testMaxAscentsLength_05()
-        {
-            Assert.Equals(1, MaxAscentsInArrays.maxAscent(new int[] { 4, 3, 2, 1 }));
-        }
+		[TestMethod]
+		public void testMaxAscentsLength_05()
+		{
+			Assert.AreEqual(1, MaxAscentsInArrays.maxAscent(new int[] { 4, 3, 2, 1 }));
+		}
 
-        [TestMethod]
-        public void testMaxAscentsLength_06()
-        {
-            Assert.Equals(2, MaxAscentsInArrays.maxAscent(new int[] { 4, 3, 2, 1, 2, 1 }));
-        }
+		[TestMethod]
+		public void testMaxAscentsLength_06()
+		{
+			Assert.AreEqual(2, MaxAscentsInArrays.maxAscent(new int[] { 4, 3, 2, 1, 2, 1 }));
+		}
 
-        [TestMethod]
-        public void testMaxAscentsLength_07()
-        {
-            Assert.Equals(3, MaxAscentsInArrays.maxAscent(new int[] { 2, 3, 4, 3, 2, 1, 2, 1 }));
-        }
+		[TestMethod]
+		public void testMaxAscentsLength_07()
+		{
+			Assert.AreEqual(3, MaxAscentsInArrays.maxAscent(new int[] { 2, 3, 4, 3, 2, 1, 2, 1 }));
+		}
 
-        [TestMethod]
-        public void testMaxAscentsLength_08()
-        {
-            Assert.Equals(4, MaxAscentsInArrays.maxAscent(new int[] { 2, 3, 4, 3, 2, 1, 2, 1, 2, 3, 4 }));
-        }
-    }
+		[TestMethod]
+		public void testMaxAscentsLength_08()
+		{
+			Assert.AreEqual(4, MaxAscentsInArrays.maxAscent(new int[] { 2, 3, 4, 3, 2, 1, 2, 1, 2, 3, 4 }));
+		}
+	}
 }
